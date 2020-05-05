ffba6cabf28cee6eb23d101f992ae014fc5a89ee
Daniel Hoyte <daniel.hoyte59@gmail.com>
2020-05-05 08:38:33 +0000

    Automatically backed up by Learn

--- lib/custom_errors.rb
+++ lib/custom_errors.rb
@@ -8,10 +8,14 @@ class Person
   def get_married(person)
     self.partner = person
     if person.class != Person
-      raise PartnerError
-    else
-    person.partner = self
-  end
+      begin
+          raise PartnerError
+        rescue PartnerError => error
+            puts error.message
+        end
+      else
+   person.partner = self
+ end
 end
 
   class PartnerError < StandardError
